e457eb9ee165a9cb3ac05934d7d931a6319d861f
imane-bouabidi <imaneyoucode@gmail.com>
2025-03-24 22:17:04 +0000

    paiment success

--- src/app/features/paiement/success/success.component.ts
+++ src/app/features/paiement/success/success.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {ActivatedRoute} from '@angular/router';
+import {AuthService} from '../../../core/services/auth/auth.service';
 
 @Component({
   selector: 'app-success',
@@ -12,15 +13,19 @@ import {ActivatedRoute} from '@angular/router';
 export class SuccessComponent  implements OnInit{
   message!: string;
 
-  constructor(private route: ActivatedRoute, private http: HttpClient) {}
+  constructor(private route: ActivatedRoute, private http: HttpClient,private authService: AuthService) {}
 
   ngOnInit(): void {
     const sessionId = this.route.snapshot.queryParamMap.get('session_id');
-    const donationId = this.route.snapshot.queryParamMap.get('donation_id');
-    this.http.get(`http://localhost:8080/gotYou/api/donations/success?session_id=${sessionId}&donation_id=${donationId}`, { responseType: 'text' })
-      .subscribe({
-        next: (response) => this.message = response,
-        error: (err) => this.message = 'Erreur : ' + err.message
+    if (sessionId) {
+      const headers = new HttpHeaders({
+        'Authorization': `Bearer ${this.authService.getToken()}`
       });
+      this.http.get(`http://localhost:8080/gotYou/api/donations/success?session_id=${sessionId}`, { headers })
+        .subscribe({
+          next: (response) => console.log('Réponse du backend :', response),
+          error: (err) => console.error('Erreur lors de l’appel à /success :', err)
+        });
+    }
   }
 }
